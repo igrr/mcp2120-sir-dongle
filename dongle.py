cb22e3b16d2cc12da5e582f6dbf714ab1bb2053f
Ivan Grokhotkov <ivan@espressif.com>
2020-06-07 00:54:23 +0200

    now emulating tekram instead of mcp2120

--- dongle.py
+++ dongle.py
@@ -1,16 +1,22 @@
 from nmigen import *
 from nmigen.build import *
-from nmigen_boards.icestick import *
 from nmigen.utils import bits_for
 from nmigen.cli import main_parser, main_runner
 from nmigen.back.pysim import *
 
+try:
+    from nmigen_boards.icestick import *
+
+    ONLY_SIM = 0
+except ImportError:
+    ONLY_SIM = 1
+
 BAUD_RATES = [
-    (9600, 0x87),
-    (19200, 0x8B),
-    (38400, 0x85),
-    (57600, 0x83),
-    (115200, 0x81)
+    (9600, 0x14),
+    (115200, 0),
+    (19200, 0x13),
+    (38400, 0x12),
+    (57600, 0x11),
 ]
 BAUD_COMMIT = 0x11
 
@@ -24,23 +30,31 @@ class IrDAConverter(Elaboratable):
         baud_rate_div_width = bits_for(freq // min_baud_rate)
         self.baud_rate_div_default = freq // default_baud_rate
         self.baud_rate_div = Signal(baud_rate_div_width, reset=self.baud_rate_div_default)
-        self.rx_div_cnt_max_default = freq // (default_baud_rate*16)
-        self.rx_div_cnt_max = Signal(baud_rate_div_width, reset=self.rx_div_cnt_max_default)
+
+        self.baud_rate_val = Signal(bits_for(max_baud_rate), reset=default_baud_rate)
+        self.baud_rate_idx = Signal(3, reset=0)
+        self.baud_rate_update_done = Signal()
+        self.baud_rate_update = Signal()
+        self.baud_rate_mode_prev = Signal(2)
 
         self.uart_rx = Signal()
         self.uart_rx_bit = Signal(bits_for(10))
         self.uart_rx_div_cnt = Signal(8)
         self.uart_rx_symbol_act = Signal(reset=0)
+
+        self.uart_rx_time_per_sub_bit = Signal(16, reset=freq * 16 // (default_baud_rate * 16))
+        self.uart_rx_time = Signal(32, reset=0)
+        self.uart_rx_next_sub_bit_time = Signal(32, reset=0)
+
         self.uart_rx_pos = Signal(bits_for(16 * 10))
         self.uart_rx_shaped = Signal()
-        self.uart_rx_ctrl_word = Signal(16)
+        self.uart_rx_ctrl_word = Signal(8)
 
         self.rx_bit_time = Signal(32, reset=0)
         self.irda_rx = Signal(reset=1)
         self.irda_rx_pulse_timeout = Signal(baud_rate_div_width)
         self.irda_rx_shaped = Signal(reset=1)
 
-        self.en = Signal()
         self.baud_rate_mode = Signal()
 
     def elaborate(self, platform):  # type: (ICEStickPlatform) -> Module
@@ -56,7 +70,7 @@ class IrDAConverter(Elaboratable):
             irda_rx_pulse_start.eq(~self.irda_rx & irda_rx_prev),
         ]
 
-        with m.If(irda_rx_pulse_start):
+        with m.If(irda_rx_pulse_start & ~self.uart_rx_symbol_act):
             m.d.sync += [
                 self.irda_rx_shaped.eq(0),
                 self.irda_rx_pulse_timeout.eq(self.baud_rate_div),
@@ -70,28 +84,30 @@ class IrDAConverter(Elaboratable):
         # Detect the start bit.
         uart_rx_prev = Signal(reset=1)
         uart_rx_start = Signal()
-        uart_rx_div_ovf = Signal()
         uart_rx_ctrl_word_update = Signal()
+        next_sub_bit_flag = Signal()
+        next_time = Signal(32)
 
         m.d.sync += [
             uart_rx_prev.eq(self.uart_rx),
             uart_rx_start.eq((~self.uart_rx) & uart_rx_prev & (~self.uart_rx_symbol_act)),
         ]
+        m.d.comb += [
+            next_time.eq(self.uart_rx_time + 16),
+            next_sub_bit_flag.eq(self.uart_rx_symbol_act & (next_time > self.uart_rx_next_sub_bit_time))
+        ]
+
         with m.If(uart_rx_start):
             m.d.sync += [
-                self.uart_rx_div_cnt.eq(self.rx_div_cnt_max),
                 self.uart_rx_pos.eq(0),
-                self.uart_rx_symbol_act.eq(1)
+                self.uart_rx_symbol_act.eq(1),
+                self.uart_rx_time.eq(0),
+                self.uart_rx_next_sub_bit_time.eq(self.uart_rx_time_per_sub_bit)
             ]
-
-        m.d.comb += uart_rx_div_ovf.eq(self.uart_rx_symbol_act & (self.uart_rx_div_cnt == 0))
-
-        with m.If(self.uart_rx_symbol_act):
-            with m.If(self.uart_rx_div_cnt == 0):
-                # 1/16 bit time
-                m.d.sync += self.uart_rx_div_cnt.eq(self.rx_div_cnt_max)
-            with m.Else():
-                m.d.sync += self.uart_rx_div_cnt.eq(self.uart_rx_div_cnt - 1)
+        with m.Elif(self.uart_rx_symbol_act):
+            m.d.sync += [
+                self.uart_rx_time.eq(next_time)
+            ]
 
         m.d.sync += uart_rx_ctrl_word_update.eq(0)
 
@@ -107,8 +123,11 @@ class IrDAConverter(Elaboratable):
             uart_rx_symbol_end.eq(self.uart_rx_bit == 10),
         ]
 
-        with m.If(uart_rx_div_ovf):
-            m.d.sync += self.uart_rx_pos.eq(self.uart_rx_pos + 1)
+        with m.If(next_sub_bit_flag):
+            m.d.sync += [
+                self.uart_rx_pos.eq(self.uart_rx_pos + 1),
+                self.uart_rx_next_sub_bit_time.eq(self.uart_rx_next_sub_bit_time + self.uart_rx_time_per_sub_bit)
+            ]
             with m.If(uart_rx_stop_bit):
                 # End of symbol
                 m.d.sync += self.uart_rx_symbol_act.eq(0)
@@ -119,24 +138,41 @@ class IrDAConverter(Elaboratable):
                     m.d.sync += self.uart_rx_shaped.eq(1)
                 with m.If((~uart_rx_start_bit) & (~uart_rx_stop_bit)):
                     # Update the control word (excluding the start bit)
-                    m.d.sync += self.uart_rx_ctrl_word.eq(Cat(self.uart_rx, self.uart_rx_ctrl_word))
+                    m.d.sync += self.uart_rx_ctrl_word.eq(Cat(self.uart_rx_ctrl_word[1:], self.uart_rx))
                     m.d.sync += uart_rx_ctrl_word_update.eq(1)
             with m.Elif(uart_rx_sub_bit == 11 - 1):
                 # Clear the output pulse
                 m.d.sync += self.uart_rx_shaped.eq(0)
 
-        uart_rx_ctrl_word_flipped = Signal(self.uart_rx_ctrl_word.width)
-        m.d.comb += uart_rx_ctrl_word_flipped.eq(self.uart_rx_ctrl_word[::-1])
+        with m.If(self.baud_rate_mode_prev == 0b10):
+            m.d.sync += [
+                self.uart_rx_ctrl_word.eq(0x14),
+                self.baud_rate_div.eq(Const(self.freq // 9600)),
+                self.uart_rx_time_per_sub_bit.eq(Const(self.freq * 16 // (9600 * 16))),
+                self.baud_rate_val.eq(Const(9600)),
+                self.baud_rate_idx.eq(Const(0)),
+            ]
 
-        baud_rate_mode_prev = Signal()
-        m.d.sync += baud_rate_mode_prev.eq(self.baud_rate_mode)
-        with m.If((~self.baud_rate_mode) & baud_rate_mode_prev):
+        m.d.sync += [
+            self.baud_rate_mode_prev.eq(Cat(self.baud_rate_mode_prev[1], self.baud_rate_mode)),
+            self.baud_rate_update_done.eq(0),
+        ]
+        m.d.comb += [
+            self.baud_rate_update.eq(self.baud_rate_mode_prev == 0b01)
+        ]
+
+        with m.If(self.baud_rate_update):
+            i = 0
             for br, c in BAUD_RATES:
-                with m.If(uart_rx_ctrl_word_flipped[8:16] == c):
+                with m.If(self.uart_rx_ctrl_word[0:8] == c):
                     m.d.sync += [
                         self.baud_rate_div.eq(Const(self.freq // br)),
-                        self.rx_div_cnt_max.eq(Const(self.freq // (br * 16)))
+                        self.uart_rx_time_per_sub_bit.eq(Const(self.freq * 16 // (br * 16))),
+                        self.baud_rate_val.eq(Const(br)),
+                        self.baud_rate_idx.eq(Const(i)),
+                        self.baud_rate_update_done.eq(1)
                     ]
+                i += 1
 
         return m
 
@@ -149,7 +185,6 @@ class IrDADongle(Elaboratable):
         self.uart_dtr = Signal()
         self.uart_rts = Signal()
 
-
         self.nrst = Signal()
         self.baud_rate_mode = Signal()
 
@@ -157,7 +192,6 @@ class IrDADongle(Elaboratable):
         m = Module()
 
         m.submodules.irda_converter = ResetInserter(self.nrst)(self.irda_converter)
-
 
         if platform:
             uart = platform.request("uart")
@@ -170,11 +204,12 @@ class IrDADongle(Elaboratable):
                 self.uart_rts.eq(uart.rts.i),
                 uart.tx.o.eq(self.irda_converter.irda_rx_shaped),
                 irda.tx.o.eq(self.irda_converter.uart_rx_shaped),
-                irda.en.o.eq(1)
+                irda.en.o.eq(1),
+                self.irda_converter.baud_rate_mode.eq(self.baud_rate_mode)
             ]
 
             m.d.sync += [
-                self.nrst.eq(self.uart_dtr | self.uart_rts),
+                self.nrst.eq((self.uart_dtr & self.uart_rts)),
                 self.baud_rate_mode.eq((~self.uart_dtr) & self.uart_rts)
             ]
 
@@ -186,64 +221,78 @@ class IrDADongle(Elaboratable):
                 dbg.o[0].eq(self.irda_converter.irda_rx),
                 dbg.o[1].eq(self.irda_converter.uart_rx),
                 dbg.o[2].eq(self.irda_converter.irda_rx_shaped),
-                dbg.o[3].eq(self.irda_converter.uart_rx_shaped),
-                dbg.o[4].eq(self.nrst),
-                dbg.o[5].eq(self.baud_rate_mode)
-                # dbg.o[1].eq(uart_rx_start),
-                # dbg.o[2].eq(self.uart_rx_symbol_act),
-                # dbg.o[3].eq(uart_rx_div_ovf),
-                # dbg.o[4:5].eq(self.uart_rx_div_cnt[0:1]),
-                # dbg.o[2].eq(self.uart_dtr),
-                # dbg.o[3].eq(self.uart_rts)
+                dbg.o[3].eq(self.irda_converter.baud_rate_mode),
+                dbg.o[4].eq(self.irda_converter.baud_rate_update_done),
+                dbg.o[5:8].eq(self.irda_converter.baud_rate_idx),
             ]
 
         return m
 
+
 class IrDADongeTB(Elaboratable):
     def __init__(self, freq):
         pass
+
     def elaborate(self, platform):
         pass
+
 
 if __name__ == "__main__":
     parser = main_parser()
     args = parser.parse_args()
-    if args.action == "generate":
+    if not ONLY_SIM and args.action == "generate":
         p = ICEStickPlatform()
         p.build(IrDADongle(int(p.default_clk_frequency)), do_program=True)
-    elif args.action == "simulate":
+    elif ONLY_SIM or args.action == "simulate":
         freq = 12000000
         dut = IrDADongle(freq)
         sim = Simulator(dut)
-        sim.add_clock(1.0/freq)
-        delay_9600_bittime = Delay(1/(9600))
+        sim.add_clock(1.0 / freq)
+
+
+        def out_byte(b, baud):
+            yield dut.irda_converter.uart_rx.eq(0)
+            yield Delay(1 / baud)
+            for i in range(8):
+                yield dut.irda_converter.uart_rx.eq((b >> i) & 1)
+                yield Delay(1 / baud)
+            yield dut.irda_converter.uart_rx.eq(1)
+            yield Delay(1 / baud)
+
+
         def process():
-            yield dut.uart_rx.eq(1)
-            yield Settle()
-            yield dut.uart_rx.eq(0)
-            yield delay_9600_bittime  # start
-            yield dut.uart_rx.eq(1)
-            yield delay_9600_bittime  # [0] = 0
-            yield dut.uart_rx.eq(1)
-            yield delay_9600_bittime  # [1] = 1
-            yield dut.uart_rx.eq(1)
-            yield delay_9600_bittime  # [2] = 0
-            yield dut.uart_rx.eq(1)
-            yield delay_9600_bittime  # [3] = 1
-            yield dut.uart_rx.eq(0)
-            yield delay_9600_bittime  # [4] = 0
-            yield dut.uart_rx.eq(0)
-            yield delay_9600_bittime  # [5] = 1
-            yield dut.uart_rx.eq(0)
-            yield delay_9600_bittime  # [6] = 0
-            yield dut.uart_rx.eq(0)
-            yield delay_9600_bittime  # [7] = 1
-            yield dut.uart_rx.eq(1)
-            yield delay_9600_bittime  # stop
-            yield dut.uart_rx.eq(1)
-            yield delay_9600_bittime  # one more
+            yield dut.irda_converter.uart_rx.eq(1)
+            yield dut.nrst.eq(1)
+            yield dut.irda_converter.baud_rate_mode.eq(0)
+            yield Delay(2/freq)
+            yield dut.nrst.eq(0)
+            yield Delay(2/freq)
+            baud = 9600
+            yield dut.irda_converter.baud_rate_mode.eq(1)
+            yield Delay(4 / freq)
+            yield dut.irda_converter.baud_rate_mode.eq(0)
+            yield Delay(4 / freq)
+            yield from out_byte(0x5a, baud)
+            yield Delay(2/freq)
+            yield dut.irda_converter.baud_rate_mode.eq(1)
+            yield Delay(2/freq)
+            yield from out_byte(0x00, baud)
+            yield Delay(2/freq)
+            yield dut.irda_converter.baud_rate_mode.eq(0)
+            baud = 115200
+            yield Delay(2/freq)
+            yield from out_byte(0x55, baud)
+            yield Delay(2/freq)
+            yield dut.irda_converter.baud_rate_mode.eq(1)
+            yield Delay(4 / freq)
+            baud = 9600
+            yield from out_byte(0x14, baud)
+            yield Delay(4 / freq)
+            yield dut.irda_converter.baud_rate_mode.eq(0)
+            yield Delay(4 / freq)
+            yield from out_byte(0x55, baud)
+
 
         sim.add_process(process)
         with sim.write_vcd(vcd_file=args.vcd_file, gtkw_file=args.gtkw_file):
-            sim.run_until(11/(9600), run_passive=True)
-
+            sim.run_until(6 * 11 / (9600), run_passive=True)
